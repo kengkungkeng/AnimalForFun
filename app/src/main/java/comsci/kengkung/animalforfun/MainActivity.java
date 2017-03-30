498df30988ef7e84edf5c5355a92703c861557dd
kengkungkeng <kengkungkeng@gmail.com>
2017-03-30 11:30:50 +0700

    Game Complete(ActivityMain)

--- app/src/main/java/comsci/kengkung/animalforfun/MainActivity.java
+++ app/src/main/java/comsci/kengkung/animalforfun/MainActivity.java
@@ -1,7 +1,11 @@
 package comsci.kengkung.animalforfun;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button clickButton = (Button) findViewById(R.id.btnStart);
+        clickButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Let's Go!",Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(MainActivity.this,Game.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
