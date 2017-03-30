d439f1fe4c9907542285eaf8e6785ccccd7b9c1a
kengkungkeng <kengkungkeng@gmail.com>
2017-03-30 11:58:24 +0700

    StartGame Complete

--- app/src/main/java/comsci/kengkung/animalforfun/MainActivity.java
+++ app/src/main/java/comsci/kengkung/animalforfun/MainActivity.java
@@ -5,22 +5,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+        String nameString;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button clickButton = (Button) findViewById(R.id.btnStart);
+        final EditText editText = (EditText) findViewById(R.id.txtname);
+
+
         clickButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Let's Go!",Toast.LENGTH_SHORT).show();
+                if (editText.length() == 0) {
+                    Toast.makeText(getApplicationContext(), "Enter Your Name Please", Toast.LENGTH_SHORT).show();
 
-                Intent intent = new Intent(MainActivity.this,Game.class);
-                startActivity(intent);
+                } else { nameString = editText.getText().toString().trim();
+                    //Toast.makeText(getApplicationContext(), "Let's Go!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Hello "+nameString, Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, Game.class);
+                    startActivity(intent);
+                }
             }
         });
     }
